b475b79f5a56fa9f2ad69da689b51f93e481e6dc
fhrrutedf <YOUR_GITHUB_EMAIL@example.com>
2026-07-29 18:13:58 -0700

    fix: add babel plugins to transform private class fields for Hermes compatibility

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
+    plugins: [
+      // Transform private class fields (#x, #y, #width, #height, etc.)
+      // required because Hermes does not support them natively
+      ["@babel/plugin-proposal-class-properties", { loose: true }],
+      ["@babel/plugin-proposal-private-methods", { loose: true }],
+    ],
   };
 };
